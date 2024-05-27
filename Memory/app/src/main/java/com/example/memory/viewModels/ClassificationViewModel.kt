a26df5345cdf1021844134feac30577448295f45
Marea99 <instimarta@gmail.com>
2024-05-27 12:48:57 +0200

    ClassificationView

--- Memory/app/src/main/java/com/example/memory/viewModels/ClassificationViewModel.kt
+++ Memory/app/src/main/java/com/example/memory/viewModels/ClassificationViewModel.kt
@@ -1,27 +1,61 @@
 package com.example.memory.viewModels
 
+import android.util.Log
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.memory.data.Repository
 import com.example.memory.data.gamePoints.GamePoints
+import com.example.memory.models.MemoryDificulty
 import kotlinx.coroutines.launch
 
 class ClassificationViewModel: ViewModel() {
     private val repository = Repository()
 
+    val difficulties = MemoryDificulty.entries
+        .map { it.name }
+        .plus("All")
+        .toList()
+
     private val _registers = MutableLiveData<List<GamePoints>>()
     val registers = _registers
 
-    fun getAllRegisters() {
+    private val _showDifClassification = MutableLiveData("All")
+    val showDifClassification = _showDifClassification
+
+    fun getRegisters(difficulty: String) {
+        Log.i("GET_REGISTERS", difficulty)
         viewModelScope.launch {
-            _registers.value = repository.getAllGamePointsRegisters()
+            when (difficulty) {
+                difficulties[0] ->
+                    _registers.value = repository.getEasyGamePointsRegisters()
+                difficulties[1] ->
+                    _registers.value = repository.getMediumGamePointsRegisters()
+                difficulties[2] ->
+                    _registers.value = repository.getHardGamePintsRegisters()
+                difficulties[3] ->
+                    _registers.value = repository.getAllGamePointsRegisters()
+            }
         }
     }
 
-    fun deleteAll() {
+    fun deleteAllRegisters() {
         viewModelScope.launch {
             repository.deleteAllGamePointsRegisters()
         }
+    }
+
+    fun deleteRegisterById(id: Int) {
+        viewModelScope.launch {
+            repository.deleteGamePintsRegisterById(id)
+        }
+
+        getRegisters(_showDifClassification.value!!)
+    }
+
+    fun setShowDiffClassification(difficulty: String) {
+        _showDifClassification.value = difficulty
     }
 }
--- Memory/app/src/main/java/com/example/memory/views/ClassificationView.kt
+++ Memory/app/src/main/java/com/example/memory/views/ClassificationView.kt
@@ -1,32 +1,118 @@
 package com.example.memory.views
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material.icons.twotone.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.memory.data.gamePoints.GamePoints
 import com.example.memory.viewModels.ClassificationViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ClassificationView(navController: NavController, viewModel: ClassificationViewModel) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Clasificaciónes") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = Icons.Default.ArrowBack.name,
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                            .clickable {
+                                navController.popBackStack()
+                            }
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            viewModel.getRegisters("All")
+            BottomNavigationBar(viewModel)
+        }
+    ) {
+        ClassificationBodyView(it, viewModel)
+    }
+}
+
+@Composable
+fun BottomNavigationBar(viewModel: ClassificationViewModel) {
+    val showDifClassification by viewModel.showDifClassification.observeAsState("All")
+    val difficultiesColor = mapOf(
+        viewModel.difficulties[0] to Color(0xFF4CAF50),
+        viewModel.difficulties[1] to Color(0xFFFFC107),
+        viewModel.difficulties[2] to Color(0xFFF44336),
+        viewModel.difficulties[3] to MaterialTheme.colorScheme.onSurface
+    )
+
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surface
+    ) {
+        viewModel.difficulties.forEach { item ->
+            NavigationBarItem(
+                selected = showDifClassification == item,
+                onClick = {
+                    viewModel.setShowDiffClassification(item)
+                    viewModel.getRegisters(showDifClassification)
+                    Log.i("ShowDiff", showDifClassification)
+                },
+                icon = { Icon(
+                    imageVector = Icons.Rounded.Star,
+                    contentDescription = "",
+                    tint = difficultiesColor[item] ?: MaterialTheme.colorScheme.onSurface
+                ) },
+                label = { Text(text = item) },
+                alwaysShowLabel = false
+            )
+        }
+    }
+}
+
+@Composable
+fun ClassificationBodyView(paddingValues: PaddingValues, viewModel: ClassificationViewModel) {
     val registers: List<GamePoints> by viewModel.registers.observeAsState(emptyList())
-    viewModel.getAllRegisters()
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(paddingValues)
             .padding(8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -42,12 +128,17 @@ fun ClassificationView(navController: NavController, viewModel: ClassificationVi
                         .fillMaxWidth()
                         .padding(8.dp),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly
+                    horizontalArrangement = Arrangement.SpaceAround
                 ) {
-                    Text(text = "Id: ${register.id}")
                     Text(text = register.name)
                     Text(text = register.difficulty.toString())
                     Text(text = register.points.toString())
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = "Delete",
+                        modifier = Modifier
+                            .clickable { viewModel.deleteRegisterById(register.id) }
+                    )
                 }
             }
         }
